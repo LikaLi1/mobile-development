1ca99a2a39c4dbd69ad2ae46a6e44e5ce5d9ffb5
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-22 14:33:00 +0300

    pr2.cs

---
+++ pr2.cs
@@ -0,0 +1,79 @@
+using System;
+using System.ComponentModel.Design;
+
+class Proga
+{
+    static void Main()
+    {
+        var userBalance = 10000;
+        var userBalance2 = 10000;
+        int money;
+        
+        while (true)
+        {
+            Console.WriteLine("Внесите сумму, которую хотите внести: ");
+            var userInputString = Console.ReadLine();
+            if (int.TryParse(userInputString, out money))
+            {
+                Console.WriteLine("Вы ввели неверное значение!");
+                continue;
+            }
+            break;
+        }
+
+        var moneyToDeposit = money;
+        userBalance += moneyToDeposit;
+
+        while (true)
+        {
+            Console.WriteLine("Внесите сумму, которую хотите снять: ");
+            var userInputString2 = Console.ReadLine();
+            if (!int.TryParse(userInputString2, out money))
+            {
+                Console.WriteLine("Вы ввели неверное значение!");
+                continue;
+            }
+            break;
+        }
+
+        var moneyToTakeOff = money;
+        if(moneyToTakeOff > userBalance)
+        {
+            Console.WriteLine("Недостаточно средств!");
+        }
+        else
+        {
+            userBalance -= moneyToTakeOff;
+        }
+
+
+        while (true)
+        {
+            Console.WriteLine("Внесите сумму, которую хотите перевести: ");
+            var userInputString3 = Console.ReadLine();
+            if (!int.TryParse(userInputString3, out money))
+            {
+                Console.WriteLine("Вы ввели неверное значение!");
+                continue;
+            }
+            else if (!int.TryParse(userInputString3, out money))
+            {
+                Console.WriteLine("Вы ввели неверные данные второго пользователя!");
+            }
+                break;
+            }
+
+            var moneyToSend = money;
+            if (moneyToSend > userBalance)
+            {
+                Console.WriteLine("Недостаточно средств!");
+            }
+            else
+            {
+                userBalance -= moneyToSend;
+                userBalance2 += moneyToSend;
+            }
+
+        }
+    }
+}
