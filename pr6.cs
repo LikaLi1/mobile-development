c0e80281f81961f56f7a50a35d2c308cc7115b01
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-03-04 11:29:43 +0300

    pr6.cs

--- pr6.cs
+++ pr6.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 
 class Task
 {
-    public int Number {  get; set; }
+    public int Number { get; set; }
     public string Description { get; set; }
-    public bool Done {  get; set; }
+    public bool Done { get; set; }
 }
 
 class proga
@@ -14,6 +14,7 @@ class proga
     {
         List<Task> tasks = new List<Task>();
         bool exit = false;
+        int taskCounter = 1;
 
         while (!exit)
         {
@@ -21,7 +22,38 @@ class proga
             "1. Добавить задачу" +
             "2. Удалить задачу" +
             "3. Редактировать задачу" +
-            "4. Удалить задачу");
+            "4. Показать все задачи" +
+            "5. Фильтр по задачам");
+        }
+
+        Console.Write("Введите номер команды: ");
+        string input = Console.ReadLine().Trim();
+
+        switch (input)
+        {
+            case "1":
+                AddTask();
+                break;
+            case "2":
+                DeleteTask();
+                break;
+            case "3":
+                EditTask();
+                break;
+            case "4":
+                ShowTask();
+                break;
+            case "5":
+                FilterTask();
+                break;
+            default:
+                Console.WriteLine("Неправильная команда, попробуйте еще раз.");
+                break;
         }
     }
+    static void AddTask()
+    {
+        Task task = new Task();
+    }
 }
+
